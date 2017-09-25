d3b1193fc60102b9e78bfbb1175064e65531e1f2
sanjeev051996 <sanjeev051996@gmail.com>
2017-09-25 13:58:57 +0530

    added user model spec test files

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do |f|
-    f.name "sky"                  #{ Faker::Name.name }
+    f.name "sudeep sharma"  #{ Faker::Name.name }
     f.email "sanj@gmail.com"
     f.password "123456"
     f.phone "5642"
@@ -12,10 +12,20 @@ FactoryGirl.define do
     f.admin false
     f.dob DateTime.now
   end
+
   factory :invalid_user, parent: :user do |f|
     f.password nil
   end
+
   factory :admin_user, parent: :user do |f|
     f.admin true
+  end
+
+  factory :invalid_name, parent: :user do |f|
+    f.name nil
+  end
+
+  factory :invalid_email, parent: :user do |f|
+    f.email "gsadfgy354@"
   end
 end
